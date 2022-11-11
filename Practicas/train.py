aa0b05a4f16a16fae7a8fdb68110f9306d3d5446
anblaca <78093167+anblaca@users.noreply.github.com>
2022-11-11 10:51:55 +0100

    all

--- Practicas/train.py
+++ Practicas/train.py
@@ -14,7 +14,15 @@ N,L=tr.shape; D=L-1;
 Xtr=tr[:,1:D]; xltr=tr[:,-1]; 
 Xdv=dv[:,1:D]; xldv=dv[:,-1];
 
-clf = LogisticRegression().fit(Xtr, xltr);
+#Parametros LogisticRegression y métodos a variar
+#1. penalty (default l2), l1,elasticnet
+#2. false
+#3. tolerance for stopping, default = 1e-4, usaremos, 1e-2, 1e-3, 1e-1
+#4 C regularization, C = 0.1, 0.5, 0.6, 1.5
+#5 class_weight default = balanced, use= None
+#solver (optimization) l1-liblinear, l2-newton-cg, l2-lbfgs, liblinear-l2, saga-elasticnet
+
+clf = LogisticRegression(penalty = 'l2', solver = 'lbfgs').fit(Xtr, xltr);
 
 probs = clf.predict_proba(Xdv)[:,1];
 auc = metrics.roc_auc_score(xldv,probs);
@@ -24,3 +32,10 @@ estimated_xldv = clf.predict(Xdv);
 err = (xldv != estimated_xldv).sum()/Xdv.shape[0]; 
 r=1.96*math.sqrt(err*(1-err)/Xdv.shape[0]);
 print("Dev CER: %.2f%% [%.2f, %.2f]" % (err*100,(err-r)*100,(err+r)*100));
+
+
+f = open("ResultsPolimedia.txt", 'a')
+f.write("----------------" + "\n")
+f.write("penalty l2, solver = lbfgs, class_weight = None" + "\n")
+f.write("Dev AUC: %.1f%%" % (auc*100) + "\n")
+f.write("Dev CER: %.2f%% [%.2f, %.2f]" % (err*100,(err-r)*100,(err+r)*100) + "\n")
